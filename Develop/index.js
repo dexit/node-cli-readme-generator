0ee5e169cf84d50e7034c939d1e9d4ba4a1b710a
Rihards Mantejs <dexit@dyc.lv>
2023-03-30 19:01:14 +0100

    ANd adding index.js extended starter code

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your REPO ?",
+         },
+         {
+            type: 'editor',
+            name: 'bio',
+            message: 'Please write a short bio of at least 3 lines.',
+            validate(text) {
+              if (text.split('\n').length < 3) {
+                return 'Must be at least 3 lines.';
+              }
+        
+              return true;
+            },
+            waitUserInput: true,
+          },
+];
+
+
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+  });
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
