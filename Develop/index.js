86ff5172cbe35a01176d7dd6cca86af6b8c6c4a5
Rihards Mantejs <dexit@dyc.lv>
2023-03-30 21:45:07 +0100

    Updated the Questions list

--- Develop/index.js
+++ Develop/index.js
@@ -4,16 +4,18 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
+
+
 const questions = [
-    {
-        type: "input",
-        name: "title",
-        message: "What is the name of your REPO ?",
-         },
-         {
-            type: 'editor',
-            name: 'bio',
-            message: 'Please write a short bio of at least 3 lines.',
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+     type: 'editor',
+    name: 'description',
+    message: 'Please write a short description of at least 3 lines. New Text editor window will open to finish close it',
             validate(text) {
               if (text.split('\n').length < 3) {
                 return 'Must be at least 3 lines.';
@@ -22,11 +24,44 @@ const questions = [
               return true;
             },
             waitUserInput: true,
-          },
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage information for your project:'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines for your project:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide test instructions for your project:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license for your project:',
+    choices: ['MIT', 'Apache', 'GPL', 'BSD']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  }
 ];
-
-
-
 inquirer.prompt(questions).then((answers) => {
     console.log(JSON.stringify(answers, null, '  '));
   });
